a01fa25e065cf3932189358ed63430cec8f36609
Jorge196 <JoRamirez96@gmail.com>
2021-01-20 13:38:54 -0500

    review class in the models.js file was added

--- JS/models.js
+++ JS/models.js
@@ -3,4 +3,27 @@ class Review {
         let whitelist = ["game_id", "stars", "active"]
         whitelist.forEach(attr => this[attr] = attributes[attr])
     }
+
+    static container(){
+        return this.c ||= document.querySelector("#reviewContainer")
+    }
+
+    static all(){
+        return fetch("http://localhost:3000/games", {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => {
+                if (res.ok) {
+                   return res.json() 
+                } else {
+                    return res.text().then(error => Promise.reject(error))
+                }
+            })
+            .then(reviewArray => {
+                debugger
+            })
+    }
 }
